5bf5e088de6d57123078970f0fb82e6fbaeb4084
Christian Dupuis <cd@atomist.com>
2018-03-20 14:21:30 +0100

    update client and lifecycle to latest versions (#95)

--- src/atomist.config.ts
+++ src/atomist.config.ts
@@ -1,5 +1,4 @@
-import * as appRoot from "app-root-path";
-import {QMConfig} from "./config/QMConfig";
+import { QMConfig } from "./config/QMConfig";
 import {
     BitbucketProjectAddedEvent,
     BitbucketProjectRequestedEvent,
@@ -8,46 +7,48 @@ import {
     ListExistingBitbucketProject,
     NewBitbucketProject,
 } from "./gluon/bitbucket/BitbucketProject";
-import {BitbucketProjectAdded} from "./gluon/bitbucket/BitbucketProjectAdded";
-import {BitbucketProjectRequested} from "./gluon/bitbucket/BitbucketProjectRequested";
-import {KickOffJenkinsBuild} from "./gluon/jenkins/JenkinsBuild";
-import {OnboardMember} from "./gluon/member/Onboard";
-import {AddSlackDetails, Whoami} from "./gluon/member/Slack";
-import {TeamMemberCreated} from "./gluon/member/TeamMemberCreated";
-import {TeamMemberCreatedEvent} from "./gluon/member/teamMemberIngester";
-import {ApplicationCreated} from "./gluon/packages/ApplicationCreated";
-import {ApplicationCreatedEvent} from "./gluon/packages/applicationsIngester";
+import { BitbucketProjectAdded } from "./gluon/bitbucket/BitbucketProjectAdded";
+import { BitbucketProjectRequested } from "./gluon/bitbucket/BitbucketProjectRequested";
+import { KickOffJenkinsBuild } from "./gluon/jenkins/JenkinsBuild";
+import { OnboardMember } from "./gluon/member/Onboard";
+import { AddSlackDetails, Whoami } from "./gluon/member/Slack";
+import { TeamMemberCreated } from "./gluon/member/TeamMemberCreated";
+import { TeamMemberCreatedEvent } from "./gluon/member/teamMemberIngester";
+import { ApplicationCreated } from "./gluon/packages/ApplicationCreated";
+import { ApplicationCreatedEvent } from "./gluon/packages/applicationsIngester";
 import {
     CreateApplication,
     LinkExistingApplication,
 } from "./gluon/packages/CreateApplication";
-import {LinkExistingLibrary} from "./gluon/packages/CreateLibrary";
-import {AddConfigServer} from "./gluon/project/AddConfigServer";
-import {CreateOpenShiftPvc} from "./gluon/project/CreateOpenShiftPvc";
-import {CreateProject} from "./gluon/project/CreateProject";
-import {ProjectCreated} from "./gluon/project/ProjectCreated";
-import {NewProjectEnvironments} from "./gluon/project/ProjectEnvironments";
-import {ProjectEnvironmentsRequested} from "./gluon/project/ProjectEnvironmentsRequested";
+import { LinkExistingLibrary } from "./gluon/packages/CreateLibrary";
+import { AddConfigServer } from "./gluon/project/AddConfigServer";
+import { CreateOpenShiftPvc } from "./gluon/project/CreateOpenShiftPvc";
+import { CreateProject } from "./gluon/project/CreateProject";
+import { ProjectCreated } from "./gluon/project/ProjectCreated";
+import { NewProjectEnvironments } from "./gluon/project/ProjectEnvironments";
+import { ProjectEnvironmentsRequested } from "./gluon/project/ProjectEnvironmentsRequested";
 import {
     ProjectCreatedEvent,
     ProjectEnvironmentsRequestedEvent,
 } from "./gluon/project/projectIngester";
 import {
     ActionedBy,
-    BitbucketProject, GluonTeam, Project,
+    BitbucketProject,
+    GluonTeam,
+    Project,
     SlackIdentity,
 } from "./gluon/shared/sharedIngester";
-import {CreateTeam} from "./gluon/team/CreateTeam";
-import {NewDevOpsEnvironment} from "./gluon/team/DevOpsEnvironment";
-import {DevOpsEnvironmentRequested} from "./gluon/team/DevOpsEnvironmentRequested";
+import { CreateTeam } from "./gluon/team/CreateTeam";
+import { NewDevOpsEnvironment } from "./gluon/team/DevOpsEnvironment";
+import { DevOpsEnvironmentRequested } from "./gluon/team/DevOpsEnvironmentRequested";
 import {
     AddMemberToTeam,
     CreateMembershipRequestToTeam,
     JoinTeam,
 } from "./gluon/team/JoinTeam";
-import {MembershipRequestClosed} from "./gluon/team/MembershipRequestClosed";
-import {MembershipRequestCreated} from "./gluon/team/MembershipRequestCreated";
-import {TeamCreated} from "./gluon/team/TeamCreated";
+import { MembershipRequestClosed } from "./gluon/team/MembershipRequestClosed";
+import { MembershipRequestCreated } from "./gluon/team/MembershipRequestCreated";
+import { TeamCreated } from "./gluon/team/TeamCreated";
 import {
     DevOpsEnvironmentRequestedEvent,
     MembershipRequestCreatedEvent,
@@ -59,14 +60,13 @@ import {
     NewTeamSlackChannel,
 } from "./gluon/team/TeamSlackChannel";
 
-const pj = require(`${appRoot.path}/package.json`);
-
 const token = QMConfig.token;
 
 export const configuration: any = {
-    name: pj.name,
-    version: pj.version,
     teamIds: [QMConfig.teamId],
+    // running durable will store and forward events when the client is disconnected
+    // this should only be used in production envs
+    policy: process.env.NODE_ENV === "production" ? "durable" : "ephemeral",
     commands: [
         NewDevOpsEnvironment,
         NewOrUseTeamSlackChannel,
@@ -129,5 +129,14 @@ export const configuration: any = {
                 enabled: false,
             },
         },
+    },
+    logging: {
+        level: "debug",
+        file: false,
+        banner: true,
+    },
+    cluster: {
+        // This will run the client in cluster mode; master and workers
+        enabled: process.env.NODE_ENV === "production",
     },
 };
